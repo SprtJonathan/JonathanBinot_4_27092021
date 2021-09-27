d54adab5bfc2f74621b94c291a853b870eb64945
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2021-09-27 13:36:17 +0200

    Installation, croix de fermeture et variables
    Installation du projet, puis mise en fonctionnement de la fermeture du modal par clic de la croix, et déclaration des variables pour la vérification des champs du formulaire. Ajout de commentaires au code JS.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,9 +8,10 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector(".bground"); // Div d'arrière plan du formulaire
+const modalBtn = document.querySelectorAll(".modal-btn"); // Bouton "Je m'inscris"
+const formData = document.querySelectorAll(".formData"); // Formulaire
+const closeBtn = document.getElementById("close-button"); // Close modal button
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,20 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// Close modal event
+closeBtn.addEventListener("click", closeModal);
 
+// Close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+// Vérification des champs
+// Variables
+let fname = document.getElementById("first");
+let lname = document.getElementById("last");
+let email = document.getElementById("email");
+let birthdate = document.getElementById("birthdate");
+let goNumber = document.getElementById("number");
+
+let nameRegex = /^[a-zA-Z ]{2,30}$/;
