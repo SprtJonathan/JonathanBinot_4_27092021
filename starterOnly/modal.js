a1cbd1200ec1906a9a5c67e48df8c201d5a26bd0
Jonathan Binot <73881990+SprtJonathan@users.noreply.github.com>
2021-10-04 17:51:09 +0200

    Réécriture du javascript, responsive

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,14 @@ const modalBtn = document.querySelectorAll(".modal-btn"); // Bouton "Je m'inscri
 const formData = document.querySelectorAll(".formData"); // Formulaire
 const closeBtn = document.getElementById("close-button"); // Close modal button
 
+let fnameId = document.getElementById("first");
+let lnameId = document.getElementById("last");
+let emailId = document.getElementById("email");
+let birthId = document.getElementById("birthdate");
+let quantityId = document.getElementById("quantity");
+let radioBlockId = document.getElementById("localisation");
+let radioId = document.getElementsByName("location");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -27,203 +35,170 @@ closeBtn.addEventListener("click", closeModal);
 // Close modal form
 function closeModal() {
   modalbg.style.display = "none";
+
+  // Suppression des messages d'erreur
+  hideError(
+    (fnameError,
+    lnameError,
+    emailError,
+    birthdateError,
+    quantityError,
+    radioError)
+  );
 }
 
 // Vérification des champs
 
+// Création d'éléments du DOM pour les messages d'erreur
+
+// Fonction créant des messages d'erreur
+function createErrorMessage(elementErrorId, name) {
+  elementErrorId.insertAdjacentHTML(
+    "afterend",
+    "<span id='" + name + "-error' class='error-text'><span>"
+  );
+}
+
+// Fonction permettant de cacher les messages d'erreurs des champs
+function hideError(errorName) {
+  errorName.style.display = "none";
+}
+
+// Erreur du champ Prénom
+createErrorMessage(fnameId, "fname");
+
+let fnameError = document.getElementById("fname-error");
+hideError(fnameError);
+
+// Message d'erreur du champ nom de famille
+createErrorMessage(lnameId, "lname");
+
+let lnameError = document.getElementById("lname-error");
+hideError(lnameError);
+
+// Message d'erreur du champ email
+createErrorMessage(emailId, "email");
+
+let emailError = document.getElementById("email-error");
+hideError(emailError);
+
+// Message d'erreur du champ date de naissance
+createErrorMessage(birthId, "birthdate");
+
+let birthdateError = document.getElementById("birthdate-error");
+hideError(birthdateError);
+
+// Message d'erreur du champ nombre de Gameon
+createErrorMessage(quantityId, "quantity");
+
+let quantityError = document.getElementById("quantity-error");
+hideError(quantityError);
+
+// Message d'erreur des radio de localisation
+createErrorMessage(radioBlockId, "radio");
+
+let radioError = document.getElementById("radio-error");
+hideError(radioError);
+
 let formBoolean = false; // Variable booléenne permettant de valider le formulaire
 
 async function verifyForm() {
-  // Variables
-  let fnameId = document.getElementById("first");
-  let lnameId = document.getElementById("last");
-  let emailId = document.getElementById("email");
-  let goNumberId = document.querySelector('input[name="quantity"]');
-  let locationRadioId = document.querySelector(
-    'input[name="location"]:checked'
-  );
-  goNumberId.attributes["required"] = "";
+  // Validation Email par expression régulière
+  function emailRegexValidation(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
 
-  // Vérification des caractères via des regex
-  let verifyLetters = /[A-Za-z]/; // Vérification des lettres uniquement
-  let verifyNumbers = /[0-9]/; // Vérification des chiffres uniquement
-  let verifyEmail =
-    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; // Vérification de l'email
-  let verifyCharacters = /[*?":{}|<>]/; // Vérification des caractères spéciaux
+  function nameRegexValidation(name) {
+    const re = [a - zA - Z];
+    return re.test(String(name));
+  }
 
-  // Création de variables pour l'affichage des erreurs
-  let formErrorHTML =
-    "<div id='form-alert'class='alert-block'><div class='alert-danger'><strong class='alert-text'>";
-  let badValue = "Format incorrect:";
-  let badValueLetter = " - Lettres interdites";
-  let badValueFigure = " - Chiffres interdits";
-  let badValueChar = " - Caractères spéciaux interdits";
-  let badValueEmail = " Format d'email incorrect:";
-
-  // Récupération des inputs
-  let fname = fnameId.value;
-  let lname = lnameId.value;
-  let email = emailId.value;
-  let goNumber = goNumberId.value;
-  let locationRadio = null;
-
-  // Si le nombre de game on est > 0 alors on rend le choix de ville obligatoire
-  if (goNumber > 0) {
-    if (locationRadioId != null) {
-      locationRadio = locationRadioId.value;
-      console.log(locationRadio);
+  function validateFname() {
+    if (fnameId.value.length < 2) {
+      first_errMessage.textContent =
+        "Please enter 2+ characters for name field";
+      first_errMessage.style.display = "block";
+      return false;
     } else {
-      console.log("localisation vide");
-      document
-        .getElementById("localisation")
-        .insertAdjacentHTML(
-          "afterend",
-          formErrorHTML +
-            "Erreur: Merci de sélectionner une localisation" +
-            "<br>" +
-            "</strong></div></div>"
-        );
+      first_errMessage.style.display = "none";
+      return true;
     }
   }
 
-  // Vérification de la validité des différents inputs
-  // Prénom
-  let fnameCheck = checkIfEmpty(
-    fname,
-    fnameId,
-    verifyNumbers.test(fname) | verifyCharacters.test(fname),
-    badValue + badValueFigure + badValueChar
-  );
-
-  console.log(fnameCheck + "fnamecheck");
-
-  /*if (verifyEmpty.test(fname) == false) {
-    fnameCheck = checkFormInput(
-      verifyNumbers.test(fname) | verifyCharacters.test(fname),
-      true,
-      fname,
-      fnameId,
-      formErrorHTML + badValue + badValueFigure + badValueChar
-    );
-  } else {
-    fnameCheck = emptyFormInput(fnameId)
-  }*/
-
-  // Nom de famille
-  let lnameCheck = checkIfEmpty(
-    lname,
-    lnameId,
-    verifyNumbers.test(lname) | verifyCharacters.test(lname),
-    badValue + badValueFigure + badValueChar
-  );
-
-  /*let lnameCheck = checkFormInput(
-    verifyNumbers.test(lname) |
-      verifyCharacters.test(lname) |
-      verifyEmpty.test(lname),
-    true,
-    lname,
-    lnameId,
-    formErrorHTML + badValue + badValueFigure + badValueChar
-  );*/
-
-  // Email
-  let emailCheck = checkEmailFormat(
-    email,
-    emailId,
-    verifyEmail.test(email),
-    badValueEmail
-  );
-
-  console.log("retour du check email : " + emailCheck);
-
-  /*let emailCheck = checkFormInput(
-    verifyEmail.test(email),
-    false,
-    email,
-    emailId,
-    formErrorHTML + badValueEmail + badValueChar
-  );*/
-
-  // Nombre de tournois Game ON
-  let goNumberCheck = checkIfEmpty(
-    goNumber,
-    goNumberId,
-    verifyNumbers.test(goNumber) | verifyCharacters.test(goNumber),
-    badValueLetter
-  );
-  /*let goNumberCheck = checkFormInput(
-    verifyLetters.test(goNumber),
-    true,
-    goNumber,
-    goNumberId,
-    formErrorHTML + badValue + badValueLetter
-  );*/
-
-  console.log(goNumberCheck);
-
-  // Variable comptant le nombre d'erreurs
-  let errorCount = lnameCheck + fnameCheck + emailCheck + goNumberCheck;
-
-  // Si la valeur de goNumber est supérieure à 0 mais qu'aucune localisation n'est choisie, alors on renvoie une erreur
-  if (goNumber > 0 && locationRadio == null) {
-    errorCount += 1;
+  function validateLname() {
+    if (formLast.value.length < 2) {
+      last_errMessage.textContent = "Please enter 2+ characters for name field";
+      last_errMessage.style.display = "block";
+      return false;
+    } else {
+      last_errMessage.style.display = "none";
+      return true;
+    }
   }
 
-  console.log("valeur de ErrorCount = " + errorCount);
-
-  if (errorCount != 0) {
-    document
-      .getElementById("signup-form")
-      .insertAdjacentHTML(
-        "afterend",
-        formErrorHTML +
-          "Erreur: Des erreurs sont présentes dans le formulaire, veuillez les corriger" +
-          "<br>" +
-          "</strong></div></div>"
-      );
-    formBoolean = false; // Si des erreurs sont retournées alors on définit la variable comme fausse pour que le formulaire ne puisse pas être envoyé
-  } else {
-    // Construction de l'objet contenant les infos du client
-    if (goNumber > 0) {
-      user = {
-        firstName: fname,
-        lastName: lname,
-        email: email,
-        goNumber: goNumber,
-        locationRadio: locationRadio,
-      };
+  function validateEmail() {
+    if (!confirmEmail(formEmail.value)) {
+      email_errMessage.textContent = "You must enter a valid email";
+      email_errMessage.style.display = "block";
+      return false;
     } else {
-      user = {
-        firstName: fname,
-        lastName: lname,
-        email: email,
-        goNumber: goNumber,
-      };
+      email_errMessage.style.display = "none";
+      return true;
     }
-    console.log("formulaire validé");
-    formBoolean = true; // Si aucune erreur n'est retournée alors on définit la variable comme vraie pour que le formulaire ne puisse pas être envoyé
+  }
+
+  function validateBirthdate() {
+    if (!formBirthdate.value) {
+      birthdate_errMessage.textContent = "You must enter your date of birth";
+      birthdate_errMessage.style.display = "block";
+      return false;
+    } else {
+      birthdate_errMessage.style.display = "none";
+      return true;
+    }
+  }
+
+  function validateQuantity() {
+    if (formQuantity.value < 0 || formQuantity.value === "") {
+      quantity_errMessage.textContent = "You must enter a valide value";
+      quantity_errMessage.style.display = "block";
+      return false;
+    } else {
+      quantity_errMessage.style.display = "none";
+      return true;
+    }
+  }
+
+  function validateLocation() {
+    let oneIsChecked = false;
+    for (let i = 0; i < formLocation.length; i++) {
+      if (formLocation[i].checked) {
+        oneIsChecked = true;
+        break;
+      }
+    }
+
+    if (!oneIsChecked) {
+      location_errMessage.textContent = "You must choose one option";
+      location_errMessage.style.display = "block";
+      return false;
+    } else {
+      location_errMessage.style.display = "none";
+      return true;
+    }
+  }
+
+  function validateCheckbox1() {
+    if (!formCheckbox1.checked) {
+      checkbox1_errMessage.textContent =
+        "You must check to agree to terms and conditions";
+      checkbox1_errMessage.style.display = "block";
+      return false;
+    } else {
+      checkbox1_errMessage.style.display = "none";
+      return true;
+    }
   }
 }
-
-// Bouton d'achat
-let formSubmit = document.getElementById("btn-signup-submit");
-
-let user;
-
-// Fonciton éxecutée lorsque le bouton acheté est cliqué
-formSubmit.addEventListener("click", (event) => {
-  event.preventDefault();
-  verifyForm(user); // Vérification du formulaire avant l'envoi
-  console.log(user);
-  if (formBoolean == true) {
-    // Vérification de la variable booléenne définie précédemment
-    modalBtn.insertAdjacentHTML(
-      "afterend",
-      "<div id='form-alert'><div class='alert alert-primary'><strong>" +
-        "Formulaire valide" +
-        "</strong></div></div>"
-    ); // Si vrai, alors on affiche un message de confirmation pour la validation du formulaire
-  }
-  console.log(formBoolean);
-});
